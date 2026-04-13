3561675f10344466c51e9fa86ea314ea1f67319f
Mr black <alokahuja039@gmail.com>
2026-04-13 16:00:59 +0530

    Update Models.kt

--- app/src/main/java/com/happyday/app/Models.kt
+++ app/src/main/java/com/happyday/app/Models.kt
@@ -1,13 +1,18 @@
-package com.happyday.app
+package com.habittracker.app
 
 data class Habit(
     val id: String,
     val name: String,
-    val completedDates: MutableSet<String> = mutableSetOf()
+    val goalDirection: String = "do_more",
+    val trackingPeriod: String = "daily",
+    val goalCount: Int = 1,
+    val startDate: String = "",
+    val completedDates: MutableMap<String, Int> = mutableMapOf()
 )
 
 data class TodoItem(
     val id: String,
     var text: String,
-    var isDone: Boolean = false
+    var isDone: Boolean = false,
+    val createdDate: String = ""
 )
